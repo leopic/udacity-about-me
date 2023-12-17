ee8ccec97a11390ee97fb1a9ac5130f0a81596d9
Leo Picado <leonardo.picado@zenjob.com>
2023-12-17 17:54:15 +0100

    Data binding

--- app/src/main/java/com/leopicado/aboutme/MainActivity.kt
+++ app/src/main/java/com/leopicado/aboutme/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.leopicado.aboutme
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.leopicado.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Leo Picado")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.myName = myName
+
         binding.doneButton.setOnClickListener {
             addNickname(it)
         }
@@ -22,11 +25,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addNickname(view: View) {
-        binding.nicknameText.text = binding.nicknameEdit.text
-        binding.nicknameEdit.visibility = View.GONE
-        binding.doneButton.visibility = View.GONE
-        binding.nicknameText.visibility = View.VISIBLE
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            // only needed when dealing with data...
+            invalidateAll() // https://developer.android.com/reference/android/databinding/ViewDataBinding#invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
 
+        // Hides the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
---
+++ app/src/main/java/com/leopicado/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package com.leopicado.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
