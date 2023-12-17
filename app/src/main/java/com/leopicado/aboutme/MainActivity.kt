af9b87a2de862fc0eb63168f9d11a9d6e783dec8
Leo Picado <leonardo.picado@zenjob.com>
2023-12-17 16:11:27 +0100

    Toggling visibility of text input

--- app/src/main/java/com/leopicado/aboutme/MainActivity.kt
+++ app/src/main/java/com/leopicado/aboutme/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.leopicado.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.leopicado.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+
+        setContentView(binding.root)
+    }
+
+    private fun addNickname(view: View) {
+        binding.nicknameText.text = binding.nicknameEdit.text
+        binding.nicknameEdit.visibility = View.GONE
+        binding.doneButton.visibility = View.GONE
+        binding.nicknameText.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
